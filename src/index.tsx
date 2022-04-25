7eb9997aa21bbc16cfcbf84a839786eabe193948
Vu Nguyen <vu_nguyen@epam.com>
2022-04-25 20:39:49 +0700

    config settings.json file

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles.scss'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.scss";
 
 function App() {
-	return (
-		<div>
-			<button className="button" type="button">
-				Sample Button
-			</button>
-			<button className="button" type="button">
-				Sample Button
-			</button>
-			<button className="button" type="button">
-				Sample Button
-			</button>
-		</div>
-	)
+    return (
+        <div>
+            <button className="button" type="button">
+                Sample Button
+            </button>
+            <button className="button" type="button">
+                Sample Button
+            </button>
+            <button className="button" type="button">
+                Sample Button
+            </button>
+        </div>
+    );
 }
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
